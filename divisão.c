ca4de4fb39db6e7d2d431ea7295d7ee79d4077b5
Jucely Gomes <202503046255@alunos.estacio.br>
2025-03-11 03:09:24 +0000

    divisão

--- divisão.c
+++ divisão.c
@@ -1,1 +1,12 @@
+int main() {
 
+    int numero = 4, resultado;
+
+    resultado = numero % 2;
+
+    printf("A variavel resultado é: %d\n", resultado);
+    
+    if (resultado == 0) {
+        printf("o numero é par\n");
+    }
+}
